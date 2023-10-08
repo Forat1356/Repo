217099f4b0f5ea2b835a494b89846697a5dcde69
Forat1356 <honest135678@gmail.com>
2023-10-08 14:50:38 -0400

    Thomas Comit 3

--- BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("21236f17-4b1a-4c30-987c-ed3052e42550")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
